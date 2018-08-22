77cc025be1d1125b3d85371719dc22ca5694e07b
alyoshas <evelynschmitz1@gmail.com>
2018-08-21 23:37:06 -0300

    Added axios.

--- schema/schema.js
+++ schema/schema.js
@@ -1,4 +1,5 @@
 const graphql = require('graphql')
+const axios = require('axios')
 const {
   GraphQLObjectType,
   GraphQLString,
@@ -22,7 +23,8 @@ const RootQuery = new GraphQLObjectType({
       type: UserType,
       args: { id: { type: GraphQLString } },
       resolve(parentValue, args) {
-        // return _.find(users, { id: args.id })
+        return axios.get(`http://localhost:3000/users/${args.id}`)
+          .then(resp => resp.data)
       }
     }
   }
